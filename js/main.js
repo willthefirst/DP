7bb1c584d3c748f0075f1002590fc820bdb03ef9
Will Moritz <will@crowdfavorite.com>
2013-08-09 11:46:17 -0600

    added npm package.json

--- js/main.js
+++ js/main.js
@@ -11,12 +11,16 @@ var TrackConfig = {
             // Fix up for prefixing
             window.AudioContext = window.AudioContext||window.webkitAudioContext;
             this.context = new AudioContext();
-\		}
+		}
 		catch(e) {
             alert('Web Audio API is not supported in this browser');
 		}
 	}
-}
+
+    loadTrack: function() {
+        $.ajax(url, settings, settings)
+    }
+};
 
 jQuery(document).ready(function($) {
 	TrackConfig.init();
