e11738217049bc28fc2e36da53d609bab968bbb8
Will Moritz <will@crowdfavorite.com>
2013-08-09 00:20:09 -0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+var TrackConfig = {
+	init: function() {
+        this.createAudioContext();
+	},
+
+	// Global audio context variable
+	context: {},
+
+    createAudioContext: function() {
+		try {
+            // Fix up for prefixing
+            window.AudioContext = window.AudioContext||window.webkitAudioContext;
+            this.context = new AudioContext();
+\		}
+		catch(e) {
+            alert('Web Audio API is not supported in this browser');
+		}
+	}
+}
+
+jQuery(document).ready(function($) {
+	TrackConfig.init();
+});
