aa4085cbeb03528e95114e0d55d70462c9862218
Will Moritz <will@crowdfavorite.com>
2013-08-15 10:07:08 -0600

    basic toggling

--- js/main.js
+++ js/main.js
@@ -1,39 +1,64 @@
 var TrackConfig = {
     init: function() {
         this.createAudioContext();
-        this.loadTrack();
+        this.controls();
     },
 
     // Params
     track_url : 'audio/glbtm.mp3',
+
     // Global audio context variable
     context: {},
-    track: {},
+    buffer: {},
+    source: {},
 
     createAudioContext: function() {
         try {
             // Fix up for prefixing
             window.AudioContext = window.AudioContext||window.webkitAudioContext;
             this.context = new AudioContext();
+
+            // Get the track
+            var request = new XMLHttpRequest();
+            request.open('GET', TrackConfig.track_url , true);
+            request.responseType = 'arraybuffer';
+
+            // Decode asynchronously
+            request.onload = function() {
+              TrackConfig.context.decodeAudioData(request.response, function(buffer) {
+                TrackConfig.buffer = buffer;
+              });
+            };
+            request.send();
         }
         catch(e) {
             alert('Web Audio API is not supported in this browser');
         }
     },
 
-    loadTrack: function() {
-          var request = new XMLHttpRequest();
-          request.open('GET', TrackConfig.track_url , true);
-          request.responseType = 'arraybuffer';
+    playTrack: function() {
+        this.source = this.context.createBufferSource();
+        this.source.buffer = this.buffer;
+        this.source.connect(this.context.destination);
+        this.source.start(0);
+    },
 
-          // Decode asynchronously
-          request.onload = function() {
-            TrackConfig.context.decodeAudioData(request.response, function(buffer) {
-              TrackConfig.track = buffer;
-              console.log(TrackConfig.track);
-            });
-          };
-          request.send();
+    stopTrack: function() {
+        this.source.stop(0);
+    },
+
+    controls: function() {
+        var playing = false;
+        $('#track-toggle').click(function() {
+            if (playing) {
+                TrackConfig.stopTrack();
+                playing = false;
+            }
+            else {
+                TrackConfig.playTrack();
+                playing = true;
+            }
+        });
     }
 };
 
