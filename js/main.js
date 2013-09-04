79f513132e843dde59d3a148fe0ed5587ffd73e5
Will Moritz <will@crowdfavorite.com>
2013-09-04 17:20:44 -0600

    vidcapture is working

--- js/main.js
+++ js/main.js
@@ -1,11 +1,41 @@
 var TrackConfig = {
 
     init: function() {
-    }
+        this.requestVidCam();
+    },
 
+    hasGetUserMedia: function() {
+      return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
+                navigator.mozGetUserMedia || navigator.msGetUserMedia);
+    },
+
+    requestVidCam: function() {
+        var onFailSoHard = function(e) {
+           console.log('Reeeejected!', e);
+        };
+
+        window.URL = window.URL || window.webkitURL;
+        navigator.getUserMedia  = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+                                  navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+        var video = document.querySelector('video');
+
+        if (navigator.getUserMedia) {
+          navigator.getUserMedia({audio: true, video: true}, function(stream) {
+            video.src = window.URL.createObjectURL(stream);
+          }, onFailSoHard);
+        } else {
+          video.src = 'somevideo.webm'; // fallback.
+        }
+    }
 
 };
 
 jQuery(document).ready(function($) {
-    TrackConfig.init();
+    if (TrackConfig.hasGetUserMedia()) {
+      TrackConfig.init();
+
+    } else {
+      alert('getUserMedia() is not supported in your browser');
+    }
 });
