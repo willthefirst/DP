e1e498b27edccfba4fc6d5f728d686ab0c47f432
Will Moritz <will@crowdfavorite.com>
2013-09-08 01:51:48 -0600

    seemingly decent recording

--- js/main.js
+++ js/main.js
@@ -7,18 +7,63 @@ var TrackConfig = {
     bpm: 133,
 
     init: function() {
-        this.getRecordRTC();
+        this.setupCam();
     },
 
-    // Return video length in seconds
-    getInterval: function(beats, bpm) {
-        return ((beats * 60 / bpm)*1000);
-    },
-
-    getRecordRTC: function() {
+    setupCam: function() {
         $.getScript('https://www.webrtc-experiment.com/RecordRTC.js', function() {
-            TrackConfig.setUpCam();
+            var onDeny = function(e) {
+               alert("Without a webcam, this site won't do much for you :(");
+            };
+
+            window.URL = window.URL || window.webkitURL;
+            navigator.getUserMedia  = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+                                      navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+            var video = document.querySelector('.usr-dance');
+
+            if (navigator.getUserMedia) {
+              navigator.getUserMedia({video: true}, function(stream) {
+                video.src = window.URL.createObjectURL(stream);
+
+                var options = {
+                    type: 'video',
+                    video: {
+                        width: 640,
+                        height: 480
+                    },
+                    canvas: {
+                        width: 640,
+                        height: 480
+                    }
+                };
+
+                TrackConfig.recordRTC = RecordRTC(stream, options);
+                TrackConfig.controls();
+              }, onDeny);
+            }
         });
+    },
+
+    controls: function() {
+        $('.beat-4').on('click', function() {
+            TrackConfig.autoRecord( 4, TrackConfig.bpm);
+        });
+        $('.beat-8').on('click', function() {
+            TrackConfig.autoRecord( 8, TrackConfig.bpm);
+        });
+        $('.beat-16').on('click', function() {
+            TrackConfig.autoRecord( 16, TrackConfig.bpm);
+        });
+    },
+
+    autoRecord: function( beats, bpm ) {
+        TrackConfig.recordRTC.startRecording();
+        var autostop = setTimeout( function() {
+            TrackConfig.recordRTC.stopRecording(function(videoURL) {
+                window.open(videoURL);
+            });
+        }, TrackConfig.getInterval(beats, bpm));
     },
 
     hasGetUserMedia: function() {
@@ -26,51 +71,11 @@ var TrackConfig = {
                 navigator.mozGetUserMedia || navigator.msGetUserMedia);
     },
 
-    setUpCam: function() {
-        var onDeny = function(e) {
-           alert("Without a webcam, this site won't do much for you :(");
-        };
-
-        window.URL = window.URL || window.webkitURL;
-        navigator.getUserMedia  = navigator.getUserMedia || navigator.webkitGetUserMedia ||
-                                  navigator.mozGetUserMedia || navigator.msGetUserMedia;
-
-        var video = document.querySelector('.usr-dance');
-
-        if (navigator.getUserMedia) {
-          navigator.getUserMedia({video: true}, function(stream) {
-            video.src = window.URL.createObjectURL(stream);
-
-            var options = {
-                type: 'video',
-                video: {
-                    width: 320,
-                    height: 240
-                },
-                canvas: {
-                    width: 320,
-                    height: 240
-                }
-            };
-
-            TrackConfig.recordRTC = RecordRTC(stream, options);
-
-            TrackConfig.autoRecord($('#record-auto'), 28, TrackConfig.bpm);
-
-          }, onDeny);
-        }
-    },
-
-    autoRecord: function( selector, beats, bpm ) {
-        selector.on('click', function() {
-            TrackConfig.recordRTC.startRecording();
-            var autostop = setTimeout( function() {
-                TrackConfig.recordRTC.stopRecording(function(videoURL) {
-                    window.open(videoURL);
-                });
-            }, TrackConfig.getInterval(beats, bpm));
-        });
+    // Return video length in seconds
+    getInterval: function(beats, bpm) {
+        return ((beats * 60 / bpm)*1000);
     }
+
 };
 
 jQuery(document).ready(function($) {
