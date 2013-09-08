babac1f3574590374d69a8669466632d8c14b122
Will Moritz <will@crowdfavorite.com>
2013-09-07 21:58:27 -0600

    succesful stream recording

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 var TrackConfig = {
 
+    mediaStream: {},
+    recordRTC: {},
+
     init: function() {
-        this.requestVidCam();
+        this.getRecordRTC();
+    },
+
+    getRecordRTC: function() {
+        $.getScript('https://www.webrtc-experiment.com/RecordRTC.js', function() {
+            TrackConfig.requestVidCam();
+        });
     },
 
     hasGetUserMedia: function() {
@@ -11,7 +20,7 @@ var TrackConfig = {
 
     requestVidCam: function() {
         var onFailSoHard = function(e) {
-           console.log('Reeeejected!', e);
+           console.log('Denied!', e);
         };
 
         window.URL = window.URL || window.webkitURL;
@@ -21,20 +30,40 @@ var TrackConfig = {
         var video = document.querySelector('video');
 
         if (navigator.getUserMedia) {
-          navigator.getUserMedia({audio: true, video: true}, function(stream) {
+          navigator.getUserMedia({video: true}, function(stream) {
             video.src = window.URL.createObjectURL(stream);
+
+            var options = {
+                type: 'video',
+                video: {
+                    width: 160,
+                    height: 120
+                },
+                canvas: {
+                    width: 160,
+                    height: 120
+                }
+            };
+
+            TrackConfig.recordRTC = RecordRTC(stream, options);
+
+            $('#play').on('click', function() {
+                TrackConfig.recordRTC.startRecording();
+            });
+            $('#stop').on('click', function() {
+                TrackConfig.recordRTC.stopRecording(function(videoURL) {
+                    window.open(videoURL);
+                });
+            });
+
           }, onFailSoHard);
-        } else {
-          video.src = 'somevideo.webm'; // fallback.
         }
     }
-
 };
 
 jQuery(document).ready(function($) {
     if (TrackConfig.hasGetUserMedia()) {
       TrackConfig.init();
-
     } else {
       alert('getUserMedia() is not supported in your browser');
     }
